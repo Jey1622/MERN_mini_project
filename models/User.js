c4952a843bbab5e6023785953e581bf09cec527e
jey1622 <jeyaramanapr22@gmail.com>
2024-01-27 11:36:48 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,22 +1,22 @@
-const  mongoose  = require("mongoose")
+const mongoose = require('mongoose')
 
-const userSchema= new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    roles:[{
-        type:String,
-        default:Boolean
-    }],
-    active:{
-        type:Boolean,
-        default:true
+    roles: {
+        type: [String],
+        default: ["Employee"]
+    },
+    active: {
+        type: Boolean,
+        default: true
     }
 })
 
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', userSchema)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
- const express=require('express')
- const router=express.Router()
- const  userController=require('../controllers/userController')
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
 
- router.route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createNewUser)
-    .patch(userController.updateUsers)
-    .delete(userController.deleteUsers)
+router.use(verifyJWT)
 
-module.exports=router
+router.route('/')
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
+
+module.exports = router
